1445e6cd7129844ffa397bd44703dca498d8476b
Manattee Vilairat <manattee2998@gmail.com>
2025-12-17 11:40:37 +0700

    Entit Difined

--- src/book-category/book-category.module.ts
+++ src/book-category/book-category.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BookCategoryService } from './book-category.service';
 import { BookCategoryController } from './book-category.controller';
+import { BookCategory } from './entities/book-category.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([BookCategory])],
   controllers: [BookCategoryController],
   providers: [BookCategoryService],
 })
